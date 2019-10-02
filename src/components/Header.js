4f76d2595a4e6cd80b85c1c90000343b3bf54ee2
Tripurari Shankar <tripurari@ixigo.com>
2019-10-02 13:40:33 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
+
+const Header = (props) => (
+    <header id="header" className="alt">
+        <Link to="/" className="logo">Dr. ROHIT PANDEY</Link>
+        <nav>
+            <a className="menu-link" onClick={props.onToggleMenu} href="">Menu</a>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onToggleMenu: PropTypes.func
+}
+
+export default Header
