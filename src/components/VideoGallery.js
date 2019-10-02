82f035bc2df0f328b96f5cafd4d095a4572f5938
Tripurari Shankar <tripurari@ixigo.com>
2019-10-02 17:58:41 +0530

    added check for window

--- src/components/VideoGallery.js
+++ src/components/VideoGallery.js
@@ -4,9 +4,11 @@ class VideoGallery extends React.Component {
   constructor(props) {
     super(props);
 
-    const videoDimension = this.computeVideoDimension()
-    this.state = videoDimension
-    this.urls = ['https://www.youtube-nocookie.com/embed/XtHWzK-g0_I']
+    if (this.isBrowser = !!(typeof window)) {
+      const videoDimension = this.computeVideoDimension()
+      this.state = videoDimension
+      this.urls = ['https://www.youtube-nocookie.com/embed/XtHWzK-g0_I']
+    }
 
     this.computeVideoDimension = this.computeVideoDimension.bind(this)
   }
@@ -28,7 +30,7 @@ class VideoGallery extends React.Component {
           <header className="major">
             <h2>Our Videos</h2>
           </header>
-          {this.urls.map(url => (
+          {this.isBrowser && this.urls.map(url => (
             <div className="video">
               <iframe width={width} height={height} src={url} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
