6258590bb90e300d71ec25ae4d286b921e80d278
Tripurari Shankar <tripurari@ixigo.com>
2019-10-02 13:57:16 +0530

    Updates

---
+++ component---src-pages-404-js-c541d10050359d01e123.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{209:function(e,t,a){"use strict";a.r(t);var n=a(0),l=a.n(n),s=a(206);t.default=function(){return l.a.createElement(s.a,null,l.a.createElement("div",{id:"main",className:"alt"},l.a.createElement("section",{id:"one"},l.a.createElement("div",{className:"inner"},l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c541d10050359d01e123.js.map
